0aec9c99d8c10500549782a56783170a5e56071d
Hecan7 <7hecano@gmail.com>
2023-04-13 22:42:51 -0700

    progress

--- Public/script.js
+++ Public/script.js
@@ -1,5 +1,6 @@
 addEventListener("scroll", (event) => {});
 
+if(500<document.documentElement.clientWidth){
 onscroll = (event) => {   if(window.scrollY != 0)
   {document.getElementById('Logos').style.width= "15%";
   document.getElementById('complogo').style.margin= "0";}else{
@@ -7,6 +8,7 @@ onscroll = (event) => {   if(window.scrollY != 0)
   document.getElementById('complogo').style.margin= "1em 0em 1em 0em";
   }
 };
+}
 
 
   if(500>document.documentElement.clientWidth){
@@ -17,6 +19,39 @@ onscroll = (event) => {   if(window.scrollY != 0)
   <div id="navbar" class="navbar">
   <a href="/" id="Logos" style="width: 50%;justify-items: center;"><img  src="./Public/logo-clear.png" id="complogo" style="margin:.5em 0em .5em 0em;" class="logo" alt="cano electric"></a>
   </div>
+  <nav role="navigation" style="
+  top: 2em;
+  left:1.5em;
+  position:fixed;
+  z-index: 2;">
+  <div id="menuToggle">
+    <!--
+    A fake / hidden checkbox is used as click reciever,
+    so you can use the :checked selector on it.
+    -->
+    <input type="checkbox" />
+    
+    <!--
+    Some spans to act as a hamburger.
+    
+    They are acting like a real hamburger,
+    not that McDonalds stuff.
+    -->
+    <span></span>
+    <span></span>
+    <span></span>
+    
+    <!--
+    Too bad the menu has to be inside of the button
+    but hey, it's pure CSS magic.
+    -->
+    <ul id="menu">
+      <a href="https://construction-calculator.herokuapp.com/"><li>Construction Calculator</li></a>
+      <a href="https://construction-calculator.herokuapp.com/even-spaced"><li>Find Even Spacing</li></a>
+    </ul>
+  </div>
+</nav>
+
   `;}
 
 
